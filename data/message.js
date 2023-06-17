20ac725445f7b13c8672297e2fe0523df12ad1ee
CofCat456 <a25690756@gmail.com>
2023-06-17 18:48:45 +0800

    feat: 🐈 change message and limit node version

--- data/message.js
+++ data/message.js
@@ -1,7 +1,6 @@
 module.exports = {
-  replyMessage: '等我一下，我正在想要怎麼回覆你...',
+  replyMessage: '這邊先冷靜一下',
   errorMessage: ({ status, statusText }) =>
     `對不起，我發生 **${status} - ${statusText}** 錯誤，所以沒有辦法回覆你。`,
-  sendMaxLengthMessage:
-    '對不起 >_< 這個貼文內容已經超過特定長度了！\n所以我無法在繼續回覆你了...\n建議你另外開啟新貼文唷~',
+  sendMaxLengthMessage: '字太長了 回不了',
 };
